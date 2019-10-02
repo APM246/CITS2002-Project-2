84c334c084be6662171c75e16188c997a6161171
Arun Muthu <50038558+APM246@users.noreply.github.com>
2019-10-02 23:35:14 +0800

    mkdir()
    starting implementation of mkdir()

--- sifs-files/library/mkdir.c
+++ sifs-files/library/mkdir.c
@@ -1,8 +1,26 @@
 #include "sifs-internal.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // make a new directory within an existing volume
 int SIFS_mkdir(const char *volumename, const char *dirname)
 {
-    SIFS_errno	= SIFS_ENOTYET;
-    return 1;
+    if (access(volumename, F_OK) != 0)
+    {
+        SIFS_errno	= SIFS_ENOVOL;
+        return 1;
+    }
+
+    SIFS_DIRBLOCK new_dir; 
+    strcpy(new_dir.name, dirname);
+    new_dir.modtime = time(NULL);
+    new_dir.nentries = 0;
+    
+    
+
+    //printf("\n%s\n", new_dir.name);
+
+    return 0;
 }
---
+++ sifs-files/sifs_mkdir.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sifs.h"
+
+int main(int argc, char *argv[])
+{
+    SIFS_mkdir("okay", "dir1");
+
+    return 0;
+}
