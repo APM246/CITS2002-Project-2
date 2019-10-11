3a93cd63266c61dc9eaa25c8e53d2134e54e3c12
Arun Muthu <50038558+APM246@users.noreply.github.com>
2019-10-11 22:58:02 +0800

    Lots of content added to writefile.c

--- sifs-files/library/writefile.c
+++ sifs-files/library/writefile.c
@@ -3,17 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <string.h>
 
-// find contiguous blocks to store contents of files 
-void find_contiguous_blocks(size_t nbytes, size_t blocksize, int nblocks)
+#define NO_CONTIGUOUS_BLOCKS -1
+
+// find contiguous blocks to store contents of files - returns first blockID of data blocks 
+int find_contiguous_blocks(size_t nbytes, size_t blocksize, int nblocks, const char *volumename, int *nblocks_needed)
 {
-    int nblocks_needed = ceil(nbytes/blocksize);
-    FILE *fp = 
+    *nblocks_needed = ceil(((double) nbytes)/blocksize); printf("\nnbytes: %lu, blocksize: %lu, needed: %i\n", nbytes, blocksize, *nblocks_needed);
+    FILE *fp = fopen(volumename, "r"); 
+    char bitmap[nblocks];
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
+    fread(bitmap, sizeof(bitmap), 1, fp);
+
+    if (nblocks < *nblocks_needed + 1 + 1)
+    {
+        return NO_CONTIGUOUS_BLOCKS;
+    }
     
     for (int i = 0; i < nblocks; i++)
     {
-        
+        for (int j = 0; j < *nblocks_needed; j++)
+        {
+            if (j == *nblocks_needed - 1 && bitmap[i+j] == SIFS_UNUSED) return i;
+            else if (bitmap[i+j] != SIFS_UNUSED) break;
+        }
     }
+
+    return NO_CONTIGUOUS_BLOCKS;
 }
 
 
@@ -28,9 +45,73 @@ int SIFS_writefile(const char *volumename, const char *pathname,
         return 1;
     }    
 
-    int nblocks, blocksize, blockID;
-    get_volume_header_info(volumename, &blocksize, &nblocks);
-    change_bitmap(volumename, SIFS_FILE, &blockID, nblocks);
+    // check directory validity 
 
+    // ACCESS VOLUME INFORMATION
+    int nblocks, blocksize, fileblockID, firstblockID, nblocks_needed;
+    get_volume_header_info(volumename, &blocksize, &nblocks);
+
+    // CHANGE BITMAP TO ADD FILE BLOCK
+    change_bitmap(volumename, SIFS_FILE, &fileblockID, nblocks);
+
+    firstblockID = find_contiguous_blocks(nbytes, blocksize, nblocks, volumename, &nblocks_needed);
+
+    if (firstblockID == NO_CONTIGUOUS_BLOCKS)
+    {
+        SIFS_errno = SIFS_ENOSPC;
+        return 1;
+    }
+
+    printf("\nfirstID: %i\n", firstblockID);
+    char *file_name = find_name(pathname);
+
+    // INITIALISE FILEBLOCK
+    SIFS_FILEBLOCK fileblock;
+    memset(&fileblock, 0, sizeof(fileblock));
+    strcpy(fileblock.filenames[0], file_name); // need generalisation
+    fileblock.firstblockID = firstblockID;
+    fileblock.length = nbytes;
+    fileblock.modtime = time(NULL);
+    fileblock.nfiles++;
+    char md5[40];
+    strcpy(md5, MD5_file(file_name);
+    //strcpy(fileblock.md5, );
+
+    // WRITE THE FILEBLOCK TO THE VOLUME 
+    FILE *fp = fopen(volumename, "r+");
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + fileblockID*blocksize, SEEK_SET);
+    fwrite(&fileblock, sizeof(fileblock), 1, fp);
+
+    // WRITE THE ACTUAL FILE TO THE VOLUME
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + firstblockID*blocksize, SEEK_SET);
+    fwrite(data, nbytes, 1, fp);
+
+    // UPDATE BITMAP
+    char bitmap[nblocks];
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
+    fread(bitmap, sizeof(bitmap), 1, fp);
+    for (int i = firstblockID; i < firstblockID + nblocks_needed; i++)
+    {
+        bitmap[i] = SIFS_DATABLOCK;
+    }
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
+    fwrite(bitmap, sizeof(bitmap), 1, fp);
+
+    // UPDATE PARENT DIRECTORY - MODTIME, NENTRIES AND ENTRIES ARRAY
+    int parent_blockID = find_parent_blockID(volumename, pathname, nblocks, blocksize);
+    size_t jump = sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + parent_blockID*blocksize; // use macro
+    fseek(fp, jump, SEEK_SET);
+    SIFS_DIRBLOCK dirblock;
+    fread(&dirblock, sizeof(SIFS_DIRBLOCK), 1, fp);
+    dirblock.modtime = time(NULL);
+    dirblock.entries[dirblock.nentries].blockID = fileblockID;
+    dirblock.entries[dirblock.nentries].fileindex = fileblock.nfiles - 1;
+    dirblock.nentries++;
+    
+    // WRITE PARENT DIRECTORY TO VOLUME
+    fseek(fp, jump, SEEK_SET);
+    fwrite(&dirblock, sizeof(dirblock), 1, fp);
+
+    fclose(fp);
     return 0;
 }
--- sifs-files/sifs_writefile.c
+++ sifs-files/sifs_writefile.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sifs.h"
+#include <string.h>
+
+char *find__name(const char *pathname)
+{
+    char *directory_name;
+    if ((directory_name = strrchr(pathname, '/')) != NULL)
+    {
+        directory_name++; // move one char past '/'
+    }
+    else
+    {
+        directory_name = malloc(32);
+        strcpy(directory_name, pathname);
+    }
+    return directory_name;
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 3)
     {
         printf("\nIncorrect number of arguments\n");
         return 1;
     }
 
-
-    FILE *fp = fopen(argv[3], "r");
-    char buffer[1000]; //change - use stat to determine size 
+    
+    FILE *fp = fopen(find__name(argv[2]), "r");
+    char buffer[100]; //change - use stat to determine size 
     fread(buffer, sizeof(buffer), 1, fp);
     SIFS_writefile(argv[1], argv[2], buffer, sizeof(buffer));
 
