53805268ccf96718f0f0feee4fce2160a9698031
Arun Muthu <50038558+APM246@users.noreply.github.com>
2019-10-14 16:05:20 +0800

    Almost done rmfile()

--- sifs-files/library/helperfunctions.h
+++ sifs-files/library/helperfunctions.h
@@ -1,4 +1,5 @@
 #include "sifs-internal.h"
+#include <stdio.h>
 
 extern void get_volume_header_info(const char *volumename, int *blocksize, int *nblocks);
 extern int change_bitmap(const char *volumename, char type, int *blockID, int nblocks);
@@ -7,3 +8,5 @@ extern int find_parent_blockID(const char *volumename, const char *pathname, int
 extern int find_blockID(const char *volumename, const char *pathname, int nblocks, int blocksize);
 extern char *find_name(const char *pathname);
 extern char *extract_start_of_pathname(const char *pathname);
+extern int get_fileindex(FILE *fp, SIFS_BLOCKID parent_blockID, SIFS_BLOCKID blockID, uint32_t nentries, uint32_t nblocks, size_t blocksize);
+extern void sort_filenames(FILE *fp, char *filename, SIFS_FILEBLOCK *fileblock);
--- sifs-files/library/rmfile.c
+++ sifs-files/library/rmfile.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 // remove an existing file from an existing volume
 int SIFS_rmfile(const char *volumename, const char *pathname)
@@ -24,23 +25,87 @@ int SIFS_rmfile(const char *volumename, const char *pathname)
     FILE *fp = fopen(volumename, "r+");
     int nblocks, blocksize;
     get_volume_header_info(volumename, &blocksize, &nblocks);
-    int block_ID = find_blockID(volumename, pathname, nblocks, blocksize); 
+    int blockID = find_blockID(volumename, pathname, nblocks, blocksize); 
     fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
     char bitmap[nblocks];
     fread(bitmap, sizeof(bitmap), 1, fp);
-    SIFS_BIT type = bitmap[block_ID];
+    SIFS_BIT type = bitmap[blockID];
 
     // THROW ERROR IF PATHNAME IS NOT A FILE
     if (type != SIFS_FILE)
     {
-        if (block_ID == -1) SIFS_errno = SIFS_ENOENT;
+        if (blockID == -1) SIFS_errno = SIFS_ENOENT;
         else SIFS_errno = SIFS_ENOTFILE; // The block is a file or data block
         return 1;
     }
 
-    // CHECK IF REMOVING LAST ENTRY OF A PARTICULAR FILEBLOCK
-    //int parent_blockID = find_parent_blockID(volumename, pathname, nblocks, blocksize);
+    // OBTAIN INFO ABOUT NENTRIES 
+    int parent_blockID = find_parent_blockID(volumename, pathname, nblocks, blocksize); 
+    SIFS_DIRBLOCK dirblock;
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + parent_blockID*blocksize, SEEK_SET);
+    fread(&dirblock, sizeof(SIFS_DIRBLOCK), 1, fp);
+    int nentries = dirblock.nentries;
+
+    // UPDATE FILEBLOCK (EITHER REMOVE IF LAST ENTRY OR UPDATE VALUES)
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + blockID*blocksize, SEEK_SET);
+    SIFS_FILEBLOCK fileblock;
+    fread(&fileblock, sizeof(SIFS_FILEBLOCK), 1, fp);
+    if (fileblock.nfiles == 1)
+    {        
+        // REMOVE DATABLOCK(S) FROM VOLUME 
+        SIFS_BLOCKID firstblockID = fileblock.firstblockID; 
+        fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + firstblockID*blocksize, SEEK_SET);
+        memset(fp, 0, fileblock.length);
+
+        //UPDATE BITMAP AND WRITE TO VOLUME 
+        bitmap[blockID] = SIFS_UNUSED; // UPDATE FILEBLOCK BIT
+        uint32_t n_datablocks = ceil(((double) fileblock.length)/blocksize); 
+        for (int i = firstblockID; i < firstblockID + n_datablocks; i++)
+        {
+            bitmap[i] = SIFS_UNUSED; //UPDATE DATABLOCKS BITS
+        }
+        fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
+        fwrite(bitmap, nblocks*sizeof(SIFS_BIT), 1, fp);
+
+        // REMOVE FILEBLOCK FROM VOLUME 
+        memset(&fileblock, 0, sizeof(SIFS_FILEBLOCK));
+    }
+    else
+    {
+        char *name = fileblock.filenames[get_fileindex(fp, parent_blockID, blockID, nentries, nblocks, blocksize)];
+        // SORT FILENAMES ARRAY (shuffle down)
+        sort_filenames(fp, name, &fileblock);
+        fileblock.nfiles--;
+    }
     
+    // WRITE FILE BLOCK BACK TO VOLUME
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + blockID*blocksize, SEEK_SET);
+    fwrite(&fileblock, sizeof(SIFS_FILEBLOCK), 1, fp);
+
+    //UPDATE PARENT DIRECTORY 
+    for (int i = 0; i < nentries; i++)   
+    {
+        if (dirblock.entries[i].blockID == blockID)
+        {
+            // shuffle entries in entries array 1 spot down
+            while (i < nentries - 1)
+            { 
+                dirblock.entries[i].blockID = dirblock.entries[i+1].blockID;
+                dirblock.entries[i].fileindex = dirblock.entries[i+1].fileindex;
+                i++;
+            }
+
+            break;
+        }
+    }
+    dirblock.entries[nentries - 1].blockID = 0;   //update last spot that was shuffled down
+    dirblock.entries[nentries - 1].fileindex = 0;
+    dirblock.modtime = time(NULL);
+    dirblock.nentries--;
+
+    // WRITE BACK TO VOLUME 
+    fseek(fp, sizeof(SIFS_VOLUME_HEADER) + nblocks*sizeof(SIFS_BIT) + parent_blockID*blocksize, SEEK_SET);
+    fwrite(&dirblock, sizeof(SIFS_DIRBLOCK), 1, fp);
 
     fclose(fp);
     return 0;
